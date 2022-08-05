8412e7ba144f307e3039f85817033e2684ce7843
naftie22 <nafti.yiga@gmail.com>
2022-08-05 01:50:27 +0100

    Implementation of the Book's metadata class showwing its state

---
+++ Book.cs
@@ -0,0 +1,18 @@
+﻿namespace LinqDemo
+{
+    public class Book
+    {
+        public int Id  { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Author { get; set; }
+
+        public Book(int id, string name, double price, string author){
+            Id = id;
+            Name = name;
+            Price = price;
+            Author = author;
+
+        }
+    }
+}
